ae8d2d8fafa83e6a004a1061e236cd6224f83b2a
Tony Jones <anthnyjns@gmail.com>
2019-02-11 11:10:20 -0600

    Add files via upload
    Final homework version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,53 +1,57 @@
 import os
 import csv
 
+# define variables
+total_months = 0
+total_amount = 0
+max_increase_amount = 0
+best_month = 0
+max_decrease_amount = 0
+worst_month = 0
+
 # open csv in named folder
 csvpath = os.path.join('..', 'PyBank', 'budget_data.csv')
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    # These initial print commands are for testing - comment out once completed
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
+        
+    # output the number of rows in csv (not returning correct value)
     for row in csvreader:
-        print(row)
+        total_months += 1
 
-    # ------- main section -------
+        # Calculate the net total amount of "Profit/Losses" over the entire period
+        total_amount = total_amount + int(row[1])
+
+        # Calculate the average of the changes in "Profit/Losses" over the entire period
+        avg_change = round((total_amount / total_months), 2)
+
+        # Calculate the greatest increase in profits (date and amount) over the entire period
+        if int(row[1]) > int(max_increase_amount):
+            best_month = row[0]
+            max_increase_amount = row[1]  
+
+        # Calculate the greates decrease in losses (date and amount) over the entire period
+        if int(row[1]) < int(max_decrease_amount):
+            worst_month = row[0]
+            max_decrease_amount = row[1]
+
+    # Print results
+    f = open('PyBank_results.txt', 'w')
+
+    print("Financial Analysis", file=f)
+    print("-" * 25, file=f)
+    print(f"Total Months: {total_months}", file=f)
+    print(f"Total: $ {total_amount}", file=f) 
+    print(f"Average Change: $ {avg_change}", file=f)
+    print(f"Greatest Increase in Profits: {best_month} ($ {max_increase_amount})", file=f)
+    print(f"Greatest Decrease in Profits: {worst_month} ($ {max_decrease_amount})", file=f)
+    f.close()
 
     print("Financial Analysis")
-    print("----------------------------------")
-
-    # output the number of rows in csv (not returning correct value)
-    csv_data = list(csvreader)
-    print("Total Months: " + str(sum(csv_data)))
-
-
-    # ------- scratch code section / remove when completed -------
-    
-    # data = list(csvreader)
-    # row_count = len(data)
-    # print(row_count)
-
-    # total_months = sum(1 for row in csvreader)
-    # print(total_months)
-
-    # ------- end scratch code section -------
-
-
-    # Calculate the net total amount of "Profit/Losses" over the entire period
-    total_amount = sum(csvreader[1])
-    print("Total: " + "$" + "(total_amount)")
-
-    # Calculate the average of the changes in "Profit/Losses" over the entire period
-
-
-    # Calculate the greatest increase in profits (date and amount) over the entire period
-
-
-    # Calculate the greates decrease in losses (date and amount) over the entire period
+    print("-" * 25)
+    print(f"Total Months: {total_months}")
+    print(f"Total: $ {total_amount}") 
+    print(f"Average Change: $ {avg_change}")
+    print(f"Greatest Increase in Profits: {best_month} ($ {max_increase_amount})")
+    print(f"Greatest Decrease in Profits: {worst_month} ($ {max_decrease_amount})")
